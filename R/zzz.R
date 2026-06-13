51dec11e85ad2cf06fdf7ca2332bb40a45201e85
Daniel Noble <daniel.noble@anu.edu.au>
2026-06-13 10:54:03 +0200

    Add startup message on load to direct folks to tutorial and also added citation file

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onAttach <- function(libname, pkgname) {
+  v <- utils::packageVersion("bayesTLS")
+  packageStartupMessage(
+    "bayesTLS ", v, "\n",
+    "Please cite: Noble DWA, Arnold PA & Pottier P (2026) A flexible modelling\n",
+    "  framework for estimating thermal tolerance and sensitivity. Manuscript in\n",
+    "  preparation. Run  citation(\"bayesTLS\")  for the full entry.\n",
+    "Tutorial & online vignette: https://daniel1noble.github.io/bayesTLS/"
+  )
+}
