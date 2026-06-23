2eaf0a1265eb7a9e8f127ed39599accb53856c3f
Daniel Noble <daniel.noble@anu.edu.au>
2026-06-23 07:59:22 +0200

    Should add brms and Rstan citations too as basyesTLS is built off that

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,9 @@
     "bayesTLS ", v, "\n",
     "Please cite: Noble DWA, Arnold PA, Nakagawa S & Pottier P (2026) A flexible modelling\n",
     "  framework for estimating thermal tolerance and sensitivity. Manuscript in\n",
-    "  preparation. Run  citation(\"bayesTLS\")  for the full entry.\n\n",
+    "  preparation.\n",
+    "bayesTLS fits with brms and Stan -- please also cite brms (Burkner 2017) and the Stan\n",
+    "  backend you use (RStan or cmdstanr). Run  citation(\"bayesTLS\")  for all entries.\n\n",
     "Tutorial & online vignette: https://daniel1noble.github.io/bayesTLS/"
   )
 }
